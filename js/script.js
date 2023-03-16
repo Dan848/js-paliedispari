e66dcae4a956afa6de6a227b96d2d61536c6fab7
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-16 17:40:43 +0100

    add palindrome script

--- js/script.js
+++ js/script.js
@@ -4,14 +4,48 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-const userWord = document.getElementById("userWord");
+//Aggancio gli elementi del DOM
+const userWordBox = document.getElementById("userWord");
+const paliBtn = document.getElementById("paliBtn");
 const printPalindrome = document.getElementById("printPalindrome");
 
+//Evento al click del bottone
+paliBtn.addEventListener("click", isPalindrome);
+//Funzione collegata al click del bottone
+function isPalindrome () {
+    //Ricavo dall'input box il testo cancellando anche eventuali spazi all'inizio e alla fine della parola
+    const userWord = userWordBox.value.trim();
+    //Elimino gli spazi eventuali ad inizio e fine parola anche nella input box 
+    userWordBox.value = userWord;
+    //Controllo che la parola inserita sia solo UNA, controllando che non ci siano spazi nella variabile stringa userWord...
+    if (userWord.includes(" ")) {
+        printPalindrome.innerHTML = `<h6 class="bg-danger rounded-2 p-2">ERRORE! Scrivi UNA parola,</h6>`;
+        //...in tal caso interrompo la funzione dopo aver stampato un messaggio di
+        return;
+    }
+    //Inverto la stringa usando la funzione reverse string
+    const reverseWord = reverseString(userWord);
+    //Converto tutte le lettere in minuscolo e confronto le due stringhe stampando i relativi messaggi nel caso in cui la parola sia palindroma o no
+    if (userWord.toLowerCase() === reverseWord.toLowerCase()){
+        printPalindrome.innerHTML = `<h6 class="bg-warning rounded-2 p-2 text-black">SI! ${userWord} al contrario è ${reverseWord}</h6>`;
+    }
+    else {
+        printPalindrome.innerHTML = `<h6 class="bg-danger rounded-2 p-2 text-black">NO! ${userWord} al contrario è ${reverseWord}</h6>`;
+    }
+
+}
+
+/*Funzione per invertire una stringa
+Separa lettere e spazi col metodo split dividendo ogni carattere della stringa rendendolo un elemento di un array
+Col metodo reverse inverte gli elementi dell'array
+Infine col metodo join unisce gli elementi dell'array in un unica stringa
+*/
+function reverseString (string) {
+    return string.split("").reverse().join("");
+}
 
 
 
-
- 
 /*
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -27,8 +61,10 @@ const userOdd = document.getElementById("userOdd");
 const userNumber = document.getElementById("userNumber");
 const playBtn = document.getElementById("playBtn");
 const printEvenOdd = document.getElementById("printEvenOdd");
+
 //Evento al click del bottone
 playBtn.addEventListener("click", playEvenOdd);
+
 //Funzione collegata al click del bottone
 function playEvenOdd () {
     //Controllo se l'utente ha scelto pari o dispari...
@@ -51,6 +87,7 @@ function playEvenOdd () {
         printEvenOdd.innerHTML = `<h6 class="bg-danger rounded-2 p-2">ERRORE! Inserisci un numero tra 1 e 5</h6>`;
         return;
     }
+
     //Genero un numero a caso tra 1 e 5 per la CPU
     const cpuNum = getRndInteger(1, 5);
 
@@ -65,15 +102,18 @@ function playEvenOdd () {
         printEvenOdd.innerHTML = `<h6 class="bg-danger rounded-2 p-2 text-black">HAI PERSO! CPU ${cpuNum} + Tu ${userNum} = ${sum(cpuNum, userNum)}</h6>`;
     }
 }
+
 //Funzione per verificare se un numero è pari o dispari attraverso l'operatore modulo (0 pari 1 dispari)...
 //...in questo caso la funzione può contenere un'altra funzione che agirà sui due numeri inseriti
 function evenOdd (num1, num2, func){
     return func(num1, num2) % 2;
 }
+
 //Funzione per sommare due numeri
 function sum (num1, num2) {
     return num1 + num2;
 }
+
 //Funzione per generare un numero casuale tra due numeri, rispettivamente min e max
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
