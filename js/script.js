1b6dad2dc77911b9a367c09689f564bf2f5f2501
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-16 16:52:25 +0100

    fix script start palindrome

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@
 Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
+*/
 
+const userWord = document.getElementById("userWord");
+const printPalindrome = document.getElementById("printPalindrome");
+
+
+
+
+
+ 
+/*
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
@@ -31,15 +41,15 @@ function playEvenOdd () {
     }
     //...in caso non lo ha fatto, stampo un messaggio di errore e interrompo la funzione
     else {
-        printEvenOdd.innerHTML = `<h6 class="bg-danger rounded-2 p-2">ERRORE! Scegli se pari o dispari</h6>`
-        return
+        printEvenOdd.innerHTML = `<h6 class="bg-danger rounded-2 p-2">ERRORE! Scegli se pari o dispari</h6>`;
+        return;
     }
 
     //Ricavo il numero dalla input bar e controllo che sia un numero tra 1 e 5, se così non è interrompo la funzione per evitare errori
     const userNum = parseInt(userNumber.value);
     if(userNum > 5 || userNum < 1 || isNaN(userNum)) {
-        printEvenOdd.innerHTML = `<h6 class="bg-danger rounded-2 p-2">ERRORE! Inserisci un numero tra 1 e 5</h6>`
-        return
+        printEvenOdd.innerHTML = `<h6 class="bg-danger rounded-2 p-2">ERRORE! Inserisci un numero tra 1 e 5</h6>`;
+        return;
     }
     //Genero un numero a caso tra 1 e 5 per la CPU
     const cpuNum = getRndInteger(1, 5);
@@ -49,10 +59,10 @@ function playEvenOdd () {
     
     //Verifico se il risultato (e cioè se il numero è pari o dispari) corrisponde o meno alla scelta dell'utente e stampo il messaggio in caso di vittoria o sconfitta
     if (result === userChoice) {
-        printEvenOdd.innerHTML = `<h6 class="bg-warning rounded-2 p-2 text-black">HAI VINTO! Il computer ha scelto ${cpuNum}</h6>`
+        printEvenOdd.innerHTML = `<h6 class="bg-warning rounded-2 p-2 text-black">HAI VINTO! CPU ${cpuNum} + Tu ${userNum} = ${sum(cpuNum, userNum)}</h6>`;
     }
     else {
-        printEvenOdd.innerHTML = `<h6 class="bg-danger rounded-2 p-2 text-black">HAI PERSO! Il computer ha scelto ${cpuNum}</h6>`
+        printEvenOdd.innerHTML = `<h6 class="bg-danger rounded-2 p-2 text-black">HAI PERSO! CPU ${cpuNum} + Tu ${userNum} = ${sum(cpuNum, userNum)}</h6>`;
     }
 }
 //Funzione per verificare se un numero è pari o dispari attraverso l'operatore modulo (0 pari 1 dispari)...
